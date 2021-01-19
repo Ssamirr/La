cd59ae217c5e5356147134f40fc6de60016be0b2
Ssamirr <sardarlisamir@gmail.com>
2021-01-19 14:14:29 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $('#owl-carousel-first').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 4,
+        autoplay: false,
+        // responsive: {
+        //     769: {
+        //         nav: true,
+        //     },
+        // }
+    });
+});
